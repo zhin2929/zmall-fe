cdfa8fd731bad55e1b7773276173f1411bc442c1
zhin <zhin2929@gmail.com>
2017-10-28 01:43:41 +0800

    完成操作结果页，同时到此完成网站的common部分

---
+++ src/page/result/index.js
@@ -0,0 +1,17 @@
+/**
+ * Author: zhin
+ * Date: 2017/10/28 1:39
+ */
+
+'use strict';
+require('./index.css');
+require('page/common/nav-simple/index.js');
+var _mm = require('util/mm.js');
+
+
+$(function () {
+  var type = _mm.getUrlParam('type') || 'default',
+      $element = $('.' + type + '-success');
+  // 显示对应的提示元素
+  $element.show();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,8 @@ var config = {
   entry: {
     'common': ['./src/page/common/index.js'],
     'index': './src/page/index/index.js',
-    'user-login': './src/page/user-login/index.js'
+    'user-login': './src/page/user-login/index.js',
+    'result': './src/page/result/index.js'
   },
   output: {
     path: './dist',
@@ -69,7 +70,8 @@ var config = {
     new ExtractTextPlugin('css/[name].css'),
     // HTML模板的处理
     new HtmlWebpackPlugin(getHtmlConfig('index', '首页')),
-    new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录'))
+    new HtmlWebpackPlugin(getHtmlConfig('user-login', '用户登录')),
+    new HtmlWebpackPlugin(getHtmlConfig('result', '操作结果'))
   ]
 
 
