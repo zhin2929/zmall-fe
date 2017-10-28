d1990406ecd60b353b2cd378c53e5ff8a2066695
zhin <zhin2929@gmail.com>
2017-10-28 17:58:12 +0800

    完成商品详情页界面

--- src/service/product-service.js
+++ src/service/product-service.js
@@ -16,6 +16,17 @@ var _product = {
       success : resolve,
       error : reject
     });
+  },
+  // 获取商品详细信息
+  getProductDetail : function(productId, resolve, reject){
+    _mm.request({
+      url     : _mm.getServerUrl('/product/detail.do'),
+      data    : {
+        productId : productId
+      },
+      success : resolve,
+      error   : reject
+    });
   }
 };
 
