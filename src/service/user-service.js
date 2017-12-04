e29a99ffb48da4731d943867149f3b7a14c8a319
zhin <zhin2929@gmail.com>
2017-12-04 19:39:47 +0800

    update get user http method

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -77,7 +77,7 @@ var _user = {
   getUserInfo : function(resolve, reject){
     _mm.request({
       url     : _mm.getServerUrl('/user/get_information.do'),
-      method  : 'POST',
+      method  : 'GET',
       success : resolve,
       error   : reject
     });
@@ -106,7 +106,7 @@ var _user = {
   checkLogin: function (resolve, reject) {
     _mm.request({
       url: _mm.getServerUrl('/user/get_user_info.do'),
-      method: 'POST',
+      method: 'GET',
       success: resolve,
       error: reject
     });
